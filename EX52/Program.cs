b3ca64a373087525d8b2fc4683c9b3f10038e753
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-24 12:00:22 +0300

    EX52

---
+++ EX52/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int rows = int.Parse(Console.ReadLine());
+int columns = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for(int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageValueColumns(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sumColumnElements = 0;
+        double average = 0;
+        double count = array.GetLength(0);
+
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sumColumnElements += array[j, i];
+            average = sumColumnElements / count;
+        }
+        Console.Write($"{average:f1} ");
+    }
+}
+
+int[,] myArray = GetArray(rows, columns, 0, 9);
+PrintArray(myArray);
+Console.WriteLine();
+AverageValueColumns(myArray);
